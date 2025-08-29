0dbb87ab577416ad32a2ac2d4f27b7ccae8547ec
abbohub <timoostrom@hotmail.com>
2025-08-29 15:53:40 +0200

    Delete Abbohubb/python cleanup.py

--- Abbohubb/python cleanup.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-
-# Open een applicatiecontext
-with app.app_context():
-    # Verwijder de tijdelijke tabel
-    with db.engine.connect() as connection:
-        connection.execute("DROP TABLE IF EXISTS _alembic_tmp_abonnement")
-    print("Tabel _alembic_tmp_abonnement verwijderd.")
